4dea084a89696ef45cfa6428df45a9f0cfee5951
shridharpithik <59431429+shridharpithik@users.noreply.github.com>
2020-02-18 23:23:30 +0530

    Add files via upload

---
+++ 3index.js
@@ -0,0 +1,40 @@
+let  num=prompt("enter number")
+
+if(num% 2== 0) {
+    num=num-1;
+}
+let m=1;
+for(i=1;i<=num;i++){
+    document.write(`${m },`)
+    m=m+2;
+}
+// let  num=prompt("enter number")
+
+// let add=num+1;
+
+    // for (let i=1; i<=num; i--) {
+      
+    //     if(i% 2 !== 0) {
+       
+    //         document.write(`${i},`);
+    //   }
+    // else if(i% 2 == 0){
+    //     document.write(`${i},`);
+    // }
+    // }
+
+// var number=prompt("enter number")
+// for(var i=1;i<=number;i++)
+// {
+//     var getodd=(2*i );
+//     if(i % 2 !== 0){
+//         document.write(`${getodd} ,`) 
+//     }}
+
+// var number=prompt("enter number")
+// for(var i=1;i<=number;i++)
+// {
+//     var getodd=(2*i );
+//     if(i % 2 !== 0){
+//         document.write(`${getodd} ,`) 
+//     }}
